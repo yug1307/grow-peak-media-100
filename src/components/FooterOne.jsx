0eca9c7586e7c0708d6adcbd67d1544755ea9ef0
yug1307 <161486463+yug1307@users.noreply.github.com>
2025-06-05 15:44:56 +0530

    Update FooterOne.jsx

--- src/components/FooterOne.jsx
+++ src/components/FooterOne.jsx
@@ -13,9 +13,9 @@ const FooterOne = () => {
         <div className="company"> 
           <h3 className="text-lg text-white font-semibold mb-4">Company</h3>
           <ul className="space-y-2">
-            <li><a href="/about" className="hover:text-white">About Us</a></li>
+            <li><a href="/about" className="text-blue-500">About Us</a></li>
             {/* <li><a href="#" className="hover:text-gray-400">Testimonials</a></li> */}
-            <li><a href="/privacy-policy" className="hover:text-white">Privacy Policy</a></li>
+            <li><a href="/privacy-policy" className="text-blue-500">Privacy Policy</a></li>
           </ul>
         </div> 
 
@@ -23,10 +23,10 @@ const FooterOne = () => {
         <div className="services">
           <a href="/services"><h3 className="text-lg text-white font-semibold mb-4">Our Services</h3></a>
           <ul className="space-y-2">
-            <li><a href="#" className="hover:text-white">Video Production</a></li>
-            <li><a href="#" className="hover:text-white">Social Media Setup</a></li>
-            <li><a href="#" className="hover:text-white">Brand Launch Packages</a></li>
-            <li><a href="#" className="hover:text-white">Social Media Management</a></li>
+            <li><a href="#" className="text-blue-500">Video Production</a></li>
+            <li><a href="#" className="text-blue-500">Social Media Setup</a></li>
+            <li><a href="#" className="text-blue-500">Brand Launch Packages</a></li>
+            <li><a href="#" className="text-blue-500">Social Media Management</a></li>
           </ul>
         </div>
 
@@ -67,14 +67,14 @@ const FooterOne = () => {
  
             <li className="flex items-center space-x-2">  
               <span className="text-white"><BiPhoneCall/></span>
-              <a href="tel:+91 7888020432" className="hover:text-white">7888020432,</a>
-              <a href="tel:+91 7974472452" className="hover:text-white">7974472452,</a>
-              <a href="tel:+91 9322276364" className="hover:text-white">9322276364</a>
+              <a href="tel:+91 7888020432" className="text-blue-500">7888020432,</a>
+              <a href="tel:+91 7974472452" className="text-blue-500">7974472452,</a>
+              <a href="tel:+91 9322276364" className="text-blue-500">9322276364</a>
             </li>
 
             <li className="flex items-center space-x-2">
               <span>📧</span>
-              <a href="mailto:team@growpeakmedia.com" className="hover:text-white">team@growpeakmedia.com</a>
+              <a href="mailto:team@growpeakmedia.com" className="text-blue-500">team@growpeakmedia.com</a>
             </li>
           </ul>
         </div>

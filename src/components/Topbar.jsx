2a714a180fcb3c7921ec6a88ae1814008c504393
yug1307 <161486463+yug1307@users.noreply.github.com>
2025-07-22 17:46:34 +0530

    Update Topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -10,10 +10,10 @@ const Topbar = () => {
       <div className="container mx-auto flex items-center justify-between px-4 py-2">
         {/* Logo or Brand */}
         <div className="text-lg text-white font-bold">
-          <h2 className="mt-1.5">Grow Peak Media</h2>
+          Grow Peak Media
       
         </div>
-          <button className='md:hidden text-2xl text-white ml-auto mr-3' onClick={() => setIsOpen(!isOpen)}>☰</button>
+          <button className='md:hidden text-2xl text-white ml-auto mr-3 mb-3' onClick={() => setIsOpen(!isOpen)}>☰</button>
 
         {/* Menu Items */}
         <ul className={`flex-col md:flex-row md:flex md:justify-between md:space-x-8 space-y-2 md:space-y-0 absolute md:static right-0 top-12 bg-gray-800 text-orange-400 md:bg-transparent w-full md:w-auto z-50 transition-all duration-300 ${isOpen ? 'flex' : 'hidden'}`}>

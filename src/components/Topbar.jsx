63de1d3d9cab94483b00a4c6d3fee17cad2542c8
yug1307 <161486463+yug1307@users.noreply.github.com>
2025-09-18 13:51:02 +0530

    Update Topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -6,7 +6,7 @@ const Topbar = () => {
   const [isOpen, setIsOpen] = useState(false);
  
   return ( 
-    <div className="w-full bg-gray-800 text-white inline-block">
+    <div className="w-full bg-violet-800 text-white inline-block">
       <div className="container mx-auto flex justify-between px-4 py-2">
         {/* Logo or Brand */}
           <h2 className="mt-1.5 text-lg text-white flex font-bold">Grow Peak Media</h2>
@@ -14,7 +14,7 @@ const Topbar = () => {
           <button className='md:hidden text-2xl text-white ml-auto mr-3' onClick={() => setIsOpen(!isOpen)}>☰</button>
 
         {/* Menu Items */}
-        <ul className={`flex-col md:flex-row md:flex md:items-center md:space-x-8 space-y-2 md:space-y-0 absolute md:static right-0 top-12 bg-gray-800 text-orange-400 md:bg-transparent w-full md:w-auto z-50 transition-all duration-300 ${isOpen ? 'flex' : 'hidden'}`}>
+        <ul className={`flex-col md:flex-row md:flex md:items-center md:space-x-8 space-y-2 md:space-y-0 absolute md:static right-0 top-12 bg-violet-800 text-lime-300 md:bg-transparent w-full md:w-auto z-50 transition-all duration-300 ${isOpen ? 'flex' : 'hidden'}`}>
           <Link to="/"><div className="block px-4 py-2 hover:underline">Home</div></Link>
           <Link to="/about"><div className="block px-4 py-2 hover:underline">About Us</div></Link>
           <Link to="/services"><div className="block px-4 py-2 hover:underline">Services</div></Link>

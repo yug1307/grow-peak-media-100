a609c127dc90737e25b8eb36f904dbdb8ee1777b
yug1307 <161486463+yug1307@users.noreply.github.com>
2025-12-31 14:54:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import App from './App.jsx';
 import About from './components/About.jsx';
-import Services from './components/Services.jsx';
 import PrivacyPolicy from './components/PrivacyPolicy.jsx';
 import ContactUs from './components/ContactUs.jsx';
+
+import VideoProduction from './services/VideoProduction.jsx';
+import SocialMediaSetup from './services/SocialMediaSetup.jsx';
+import SocialMediaManagement from './services/SocialMediaManagement.jsx';
+import BrandLaunchPackages from './services/BrandLaunchPackages.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
  
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
+    <Routes>  
       <Route path="/" element={<App />} /> 
       <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-      <Route path="/contact" element={<ContactUs/>} />
+      <Route path="/privacypolicy" element={<PrivacyPolicy />} /> 
+      <Route path="/contact" element={<ContactUs/>} /> 
+      <Route path="/video-production" element={<VideoProduction/>} />
+      <Route path="/social-media-setup" element={<SocialMediaSetup/>} />
+      <Route path="/social-media-management" element={<SocialMediaManagement/>} />
+      <Route path="/brand-launch-packages" element={<BrandLaunchPackages/>} />
     </Routes>
   </BrowserRouter>
 )
